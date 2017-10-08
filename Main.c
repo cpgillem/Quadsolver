f3188f8e73da915b4a6cfd47763d88ff2138766e
Cade Gillem <cpgillem@gmail.com>
2017-10-08 16:08:21 -0400

    Add basic files.

---
+++ Main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "Quadratic.c"
+
+int main() {
+	double a, b, c;
+	double x1, x2;
+
+	printf("Enter a: \n");
+	scanf("%lf", &a);
+
+	printf("Enter b: \n");
+	scanf("%lf", &b);
+
+	printf("Enter c: \n");
+	scanf("%lf", &c);
+
+	qsolve(a, b, c, &x1, &x2);
+	printf("Solution 1: %lf\n", x1);
+	printf("Solution 2: %lf\n", x2);
+}
