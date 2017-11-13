bfffef138f00d454ba57b5203abaf0f08f8b2fc5
Cade Gillem <cpgillem@gmail.com>
2017-11-13 12:03:34 -0500

    improve error checking.

--- Main.c
+++ Main.c
@@ -4,7 +4,9 @@
 int main() {
 	double a, b, c;
 	double x1, x2;
+	int result;
 
+	// Receive input.
 	printf("Enter a: \n");
 	scanf("%lf", &a);
 
@@ -14,7 +16,14 @@ int main() {
 	printf("Enter c: \n");
 	scanf("%lf", &c);
 
-	qsolve(a, b, c, &x1, &x2);
-	printf("Solution 1: %lf\n", x1);
-	printf("Solution 2: %lf\n", x2);
+	// Calculate the roots of the quadratic equation.
+	result = qsolve(a, b, c, &x1, &x2);
+
+	// Only print the solution if there was no error.
+	if (result == 0) {
+		printf("Solution 1: %lf\n", x1);
+		printf("Solution 2: %lf\n", x2);
+	} else {
+		printf("Error %d encountered. Check log file.\n", result);
+	}
 }
